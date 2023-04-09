432241ac4be02ea4574920723756950ec72b0453
lucab <lucabombonati31@gmail.com>
2023-04-09 17:01:41 +0200

    JSON send format

--- server/app.py
+++ server/app.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
 
+import time
+
+from random import seed, random
+seed(420)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
-
-if __name__== '__main__':
-  socketio.run(app)
 
 @app.route("/greet")
 def greet():
@@ -18,7 +20,45 @@ def greet():
     }
   """
 
+@app.route("/data")
+def send():
+  # read data from SPI
+  data = {
+    "temperature": {
+      "motors": {
+        "fl": random(),
+        "fr": random(),
+        "rl": random(),
+        "rr": random()
+      }
+    },
+    "voltage": {
+      "hv": {
+        "high": random(),
+        "low": random(),
+        "avg": random(),
+      },
+      "lv": {
+        "total": random(),
+        "low": random()
+      },
+    },
+    "car": {
+      "info": {
+        "throttle": random(),
+        "steeringangle": random(),
+        "brake": random()
+      }
+    }
+  }
+  print(type(data))
+  socketio.emit('data', data)
+  return data
+
 @socketio.on('hello')
 def handle_message(data):
   print('received: ' + data)
-  socketio.emit('hello', 'client')
+  send()
+
+if __name__== '__main__':
+  socketio.run(app)
