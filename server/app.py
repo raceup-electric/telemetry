408deea24b7280ee92015277f338ab0af1c2d3fa
Tommaso Prandin <tommy.prando@gmail.com>
2023-05-06 13:26:35 +0200

    Lettura seriale su altro thread

--- server/app.py
+++ server/app.py
@@ -1,10 +1,13 @@
+import serial
+from serial.threaded import ReaderThread
 from flask import Flask
+from flask_apscheduler import APScheduler
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from reader import SerialReader
-from flask_apscheduler import APScheduler
+import globals
 
-port = '/dev/ttyACM0'
+port = '/dev/tty.usbmodem1101'
 baud = 9600
 
 app = Flask(__name__)
@@ -15,7 +18,7 @@ scheduler.start()
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-reader = SerialReader(port, baud)
+ser = serial.Serial(port, baud, timeout=1)
 
 @app.route("/greet")
 def greet():
@@ -25,43 +28,44 @@ def greet():
     }
   """
 
+@scheduler.task('interval', id='send_task', seconds=1, misfire_grace_time=5)
 def send_data():
-  # read data from serial
-  val = reader.read()
-  data = {
+  globals.lock.acquire(blocking=True)
+  print(globals.data)
+  json_data = {
     "temperature": {
       "motors": {
-        "fl": val[0],
-        "fr": val[1],
-        "rl": val[2],
-        "rr": val[3]
+        "fl": globals.data[0],
+        "fr": globals.data[1],
+        "rl": globals.data[2],
+        "rr": globals.data[3]
       }
     },
     "voltage": {
       "hv": {
-        "high": val[4],
-        "low": val[5],
-        "avg": val[6],
+        "high": globals.data[4],
+        "low": globals.data[5],
+        "avg": globals.data[6],
       },
       "lv": {
-        "total": val[7],
-        "low": val[8]
+        "total": globals.data[7],
+        "low": globals.data[8]
       },
     },
     "car": {
       "info": {
-        "throttle": val[9],
-        "steeringangle": val[10],
-        "brake": val[11]
+        "throttle": globals.data[9],
+        "steeringangle": globals.data[10],
+        "brake": globals.data[11]
       }
     }
   }
-  print(val)
-  print(data) 
-  socketio.emit('data', data)
+  globals.lock.release()
+  print(json_data)
+  socketio.emit('data', globals.data)
   
-  
-scheduler.add_job(id='send_task', func=send_data, trigger='interval', seconds=0.5)
+read_thread = ReaderThread(ser, SerialReader)
 
-if __name__== '__main__':
+if __name__ == '__main__':
+  read_thread.start()
   socketio.run(app)
--- server/reader.py
+++ server/reader.py
@@ -1,26 +1,11 @@
-import serial
+from serial.threaded import LineReader
+import globals
+import numpy
 
-class Reader:
-  def read() -> dict:
-    pass
-
-class SerialReader(Reader):
-
-  ser = serial.Serial()
-
-  def __init__(self, port: str, baud: int) -> None:
-    self.ser.port = port
-    self.ser.baudrate = baud
-    self.ser.open()
-
-  def __del__(self):
-    self.ser.close()
-
-  def read(self):
-    data = self.ser.read(24)
-    val = []
-    for _ in range(0, 12):
-      # val.append((int)(data[0:2]))
-      val.append(((int)(data[1]) << 8) + (int)(data[0]))
-      data = data[2:]
-    return val
+class SerialReader(LineReader):
+  def handle_line(self, line: str) -> None:
+    globals.lock.acquire()
+    globals.data = []
+    for elt in numpy.fromstring(line, dtype=int, sep=','):
+      globals.data.append(elt)
+    globals.lock.release()
