390817d81fb41548fd81803bb8cba7e444f8babb
lBombo <luca.bombonati@raceup.it>
2023-11-02 16:20:16 +0100

    First refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,24 @@
-import { useState, useContext, SyntheticEvent } from 'react'
+import { useState, useContext, SyntheticEvent, createElement } from 'react'
 
 import {
-  Tab,
-  Box,
+    Tab,
+    Box,
+    Tabs,
+    Stack
 } from "@mui/material/";
 
 import {
-  TabContext,
-  TabList,
-  TabPanel
+    TabContext,
+    TabPanel
 } from '@mui/lab/';
 
 import CustomPlot from './pages/CustomPlot';
-import Dashboard from './pages/DashBoard';
+import DashBoard from './pages/DashBoard';
+
+import {
+    AutoFixHigh,
+    Dashboard
+} from '@mui/icons-material';
 
 import { LOG_DEFS } from './log_defs';
 import DefaultPlots from './pages/DefaultPlots';
@@ -20,65 +26,117 @@ import { SB_CONTEXT } from './main';
 
 interface Payload {
     payload: {
-      new: { [key: string]: number };
-  }
+        new: { [key: string]: number };
+    }
 }
 
 function App() {
-  const supabase = useContext(SB_CONTEXT)!;
+    const supabase = useContext(SB_CONTEXT)!;
 
-  // Page displayed
-  const [page, setPage] = useState('dashboard');
-  const handleChange = (_event: SyntheticEvent, newPage: string) => {
-    setPage(newPage);
-  };
+    // Page displayed
+    const [page, setPage] = useState('dashboard');
+    const handleChange = (_event: SyntheticEvent, newPage: string) => {
+        setPage(newPage);
+    };
 
-  const [lastPayload, setNewPayload] = useState({new: {}});
+    const [lastPayload, setNewPayload] = useState({ new: {} });
 
-  // Supabase event
-  const ecu = supabase.channel('custom-insert-channel').on('postgres_changes', {
-    event: 'INSERT',
-    schema: 'public',
-    table: import.meta.env.VITE_SB_TABLE,
-  },
-    (payload) => {
-      setNewPayload(payload);
-    }
-  ).subscribe();
+    // Supabase event
+    const ecu = supabase.channel('custom-insert-channel').on('postgres_changes', {
+        event: 'INSERT',
+        schema: 'public',
+        table: import.meta.env.VITE_SB_TABLE,
+    },
+        (payload) => {
+            setNewPayload(payload);
+        }
+    ).subscribe();
 
-  return (
-    <div className="App">
-      <Box sx={{ width: '100%', typography: 'body1' }} />
-      <TabContext value={page}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChange} aria-label="" variant="scrollable" orientation="vertical">
-            <Tab label="Dashboard" value="dashboard" />
-            {LOG_DEFS.map((def) => (
-              <Tab key={def.id} label={def.label} value={def.id} />
-            ))}
-            <Tab label="Custom plot" value="customPlot" />
-          </TabList>
-        </Box>
-        <TabPanel value="dashboard">
-          <div className='innerBody'>
-            <Dashboard payload={lastPayload} />
-          </div>
-        </TabPanel>
-        {LOG_DEFS.map((def) => (
-          <TabPanel key={def.id} value={def.id}>
-            <div className='innerBody'>
-              <DefaultPlots jRef={def.values} payload={lastPayload}></DefaultPlots>
-            </div>
-          </TabPanel>
-        ))}
-        <TabPanel value="customPlot">
-          <div className='innerBody'>
-            <CustomPlot payload={lastPayload} />
-          </div>
-        </TabPanel>
-      </TabContext>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Stack direction="row" spacing={0} useFlexGap flexWrap="wrap" sx={{ height: '100%' }}>
+                <Box
+                    sx={{
+                        flexGrow: 1,
+                        bgcolor: 'background.paper',
+                        display: 'flex',
+                        position: 'fixed',
+                        width: '17.5vw',
+                        height: '100%',
+                        borderRight: 1,
+                        borderColor: 'divider'
+                    }}>
+                    <Stack spacing={5}>
+                        <img
+                            src={'../logo.png'}
+                            loading="lazy"
+                            style={{
+                                position: 'relative',
+                                top: '2%',
+                                width: '35%',
+                                left: '50%',
+                                transform: 'translateX(-50%)'
+                            }}
+                        />
+                        <Tabs
+                            orientation="vertical"
+                            variant="scrollable"
+                            value={page}
+                            onChange={handleChange}
+                            sx={{
+                                position: 'relative',
+                                height: '80%'
+                            }}
+                        >
+                            <Tab icon={<Dashboard />} iconPosition="start" label="Dashboard" value="dashboard" />
+                            {LOG_DEFS.map((def) => (
+                                <Tab key={def.id} icon={createElement(def.icon)} iconPosition="start" label={def.label} value={def.id} />
+                            ))}
+                            <Tab icon={<AutoFixHigh />} iconPosition="start" label="Custom plot" value="customPlot" />
+                        </Tabs>
+                    </Stack>
+                </Box>
+                <Box sx={{
+                    flexGrow: 1,
+                    bgcolor: 'background.paper',
+                    display: 'flex',
+                    marginLeft: '18vw',
+                    position: 'fixed',
+                    height: '100%'
+                }}>
+                    <TabContext value={page}>
+                        <TabPanel value="dashboard" sx={{
+                            height: '100%',
+                            overflow: 'auto'
+                        }}>
+                            <div style={{ marginTop: '5%' }}>
+                                <DashBoard payload={lastPayload} />
+                            </div>
+                        </TabPanel>
+                        {LOG_DEFS.map((def) => (
+                            <TabPanel key={def.id} value={def.id} sx={{
+                                width: '100vw',
+                                height: '100%',
+                                overflow: 'auto'
+                            }}>
+                                <div style={{ marginTop: '5%' }}>
+                                    <DefaultPlots jRef={def.values} payload={lastPayload}></DefaultPlots>
+                                </div>
+                            </TabPanel>
+                        ))}
+                        <TabPanel value="customPlot" sx={{
+                            height: '100%',
+                            overflow: 'auto'
+                        }}>
+                            <div style={{ marginTop: '5%' }}>
+                                <CustomPlot payload={lastPayload} />
+                            </div>
+                        </TabPanel>
+                    </TabContext>
+                </Box>
+            </Stack>
+        </div>
+    )
 }
 
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,15 @@
 import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 
 import { SupabaseClient, createClient } from '@supabase/supabase-js'
-import App2 from './App2'
+import App from './App'
 export const SB_CONTEXT = createContext<SupabaseClient | null>(null);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   //<React.StrictMode>
   <SB_CONTEXT.Provider value={createClient(import.meta.env.VITE_SB_URL, import.meta.env.VITE_SB_KEY)}>
-    <App2 />
+    <App />
   </SB_CONTEXT.Provider>
   //</React.StrictMode>,
 )
